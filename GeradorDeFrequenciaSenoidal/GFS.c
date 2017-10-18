3ae1877b00f622fbf8d657250d8cc28d1b86162e
Guilherme Poleto <gpoleto@alunos.utfpr.edu.br>
2017-10-18 19:51:46 -0200

    Update GFS.c

--- GeradorDeFrequenciaSenoidal/GFS.c
+++ GeradorDeFrequenciaSenoidal/GFS.c
@@ -9,14 +9,16 @@ int main ()
     slWindow(800, 600, "Gerador de Frequência Senoidal", 0);
     slSetFont(slLoadFont("arial_narrow_7.ttf"), 24);
     slSetTextAlign(SL_ALIGN_CENTER);
-    int f = 0, t=1, xG = 0, yG = 0;
+    int f = 0, t = 0, xG = 0, yG = 0;
     double x = 50.0, y = 300.0, d = 1000;
     char *freq = malloc(4*sizeof(char)), *dur = malloc(4*sizeof(char));
     while(!slShouldClose()&&!slGetKey(SL_KEY_ESCAPE))
     {
+        slSetFontSize(24);
         sprintf(dur, "%.1lf", (d/1000));
         sprintf(freq, "%d", f);
         slSetForeColor(1,1,1,1);
+        slText(125, 510, "Escala x = 1:69");
         slSetFontSize(40);
         slText (400, 550, "Gerador de Frequencia Senoidal");
         slRectangleOutline (400, 300, 700, 400);
@@ -29,6 +31,7 @@ int main ()
         slText(400, 15, "EXECUTAR");
         slText(175, 50, "+");
         slText(675, 50, "+");
+        slLine(50,300,750,300);
 
         slSetForeColor(1,0,0,1);
         slText(75, 50, "-");
@@ -36,12 +39,12 @@ int main ()
 
         while (x <= 750)
         {
-            y = 300+(175*sin(-(f*2*PI)*t));
+            y = 300+(190*sin(-(f*2*PI)*t));
             xG = (int)x;
             yG = (int)y;
             slPoint(xG,yG);
             x +=0.01;
-            t++;
+            t+=1;
         }
         x = 50.0;
         t = 0;
@@ -54,7 +57,7 @@ int main ()
 
         if (slGetMouseX()> 560 && slGetMouseX()< 585 && slGetMouseY() > 25 &&  slGetMouseY() < 75 && slGetMouseButton(SL_MOUSE_BUTTON_LEFT)) /// - ms
         {
-            if (f >= 0)
+            if (f > 0)
                 f -=1;
         }
 
@@ -71,8 +74,10 @@ int main ()
         }
 
         if (slGetMouseX()> 315 && slGetMouseX()< 475 && slGetMouseY() > 10 &&  slGetMouseY() < 50 && slGetMouseButton(SL_MOUSE_BUTTON_LEFT)) /// - ms
-            Beep(f, d);
-
+            {
+                Sleep(100);
+                Beep(f, d);
+            }
         slRender();
     }
     return 0;
